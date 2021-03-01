b531fbece31bf85e96b494fdda84339b8df488cf
Yuka Nakato <nakat013@umn.edu>
2021-03-01 15:03:44 -0800

    Add more time errors

--- plot_random_sky_slices.py
+++ plot_random_sky_slices.py
@@ -11,7 +11,7 @@ Ndishes_array = [32, 64, 128]
 Npix_array = [2**11, 2**12, 2**13]
 redundant_array = [True, False]
 sky_array = ['uniform', 'poisson', 'gaussian']
-time_error_array = [1e-12, 10e-12, 100e-12]
+time_error_array = [1e-12, 10e-12, 100e-12, 300e-12, 1e-9]
 
 for npix in Npix_array:
     for ndishes in Ndishes_array:
@@ -33,6 +33,6 @@ for npix in Npix_array:
                     rmap_with_error = t.get_obs_rmap(uvplane, time_error_sigma=error)
                     (ps_binned_with_error, k_modes_with_error, alpha_binned_with_error) = t.get_rmap_ps(rmap_with_error, Nfreqchunks=4, m_alpha=2, m_freq=2, padding=1, window_fn=np.blackman)
                     f = t.plot_rmap_ps_slice(ps_binned_no_error, ps_binned_with_error, k_modes_no_error, alpha_binned_no_error, alpha_idx_source=[], 
-                                             alpha_idx_no_source=[t.Npix//2, t.Npix//2+10, t.Npix//2+25, t.Npix//2+50], Nfreqchunks=4)
+                                             alpha_idx_no_source=[t.Npix//2, t.Npix//2+t.Npix//400, t.Npix//2+t.Npix//150, t.Npix//2+t.Npix//80], Nfreqchunks=4)
                     f.savefig(f'../20210225_random_sky_slices/figs/npix_{npix}_ndish_{ndishes}_redundant_{redundant}_sky_{sky}_error_{error}.png')
                     plt.close(f)
