ec49540fbb4bb16431f9db9785faca0632391bf3
Yuka Nakato <yukanaka@cent7c.slac.stanford.edu>
2021-03-02 00:15:01 -0800

    Make queue long not short

--- submit.sh
+++ submit.sh
@@ -4,7 +4,7 @@ for npix in 2048 4086 8192; do
         for sky in 'uniform' 'poisson' 'gaussian'; do
             for error in '1e-12' '10e-12' '100e-12' '300e-12' '1e-9'; do
                 #echo "bsub -C 0 -W 1:00 -R select[rhel60] -o logfile_${npix}_${redundant}_${sky}_${error}.txt plot_random_sky_slices.py $npix $redundant $sky $error"
-                bsub -C 0 -W 1:00 -R select[rhel60] -o logfile_${npix}_${redundant}_${sky}_${error}.txt ./plot_random_sky_slices.py $npix $redundant $sky $error
+                bsub -q long -C 0 -W 1:00 -R select[rhel60] -o logfile_${npix}_${redundant}_${sky}_${error}.txt ./plot_random_sky_slices.py $npix $redundant $sky $error
             done
         done
     done
