5bcab06a215e0cae7e83b95a1cdd2524091e9099
Yuka Nakato <yukanaka@cent7c.slac.stanford.edu>
2021-03-03 23:57:33 -0800

    Add the long queue bit

--- submit.sh
+++ submit.sh
@@ -6,7 +6,7 @@ for npix in 2048 4096 8192; do
                 for error in '1e-12' '10e-12' '100e-12' '300e-12' '1e-9'; do
                     for correlated in 'True' 'False'; do
                         #echo "bsub -C 0 -W 1:00 -R select[rhel60] -o logfile_${npix}_${redundant}_${sky}_${error}.txt plot_random_sky_slices.py $npix $redundant $sky $error"
-                        bsub -C 0 -W 1:00 -R select[rhel60] -o logfile_${npix}_${redundant}_${sky}_${seed}_${error}_${correlated}.txt ./plot_random_sky_slices.py $npix $redundant $sky $seed $error $correlated
+                        bsub -q long -C 0 -W 1:00 -R select[rhel60] -o logfile_${npix}_${redundant}_${sky}_${seed}_${error}_${correlated}.txt ./plot_random_sky_slices.py $npix $redundant $sky $seed $error $correlated
                     done
                 done
             done
