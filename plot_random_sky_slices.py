1d1a4df32c247cf1702a265abb1d68cfb8b5c428
Yuka Nakato <nakat013@umn.edu>
2021-03-05 12:57:29 -0800

    Make alphas closer to 0

--- plot_random_sky_slices.py
+++ plot_random_sky_slices.py
@@ -75,7 +75,7 @@ def plot_random_sky_slices(npix, redundant, sky, seed, error, correlated):
         rmap_diff = rmap_with_error - rmap_no_error
         (difference_ps_binned, k_modes_diff, alpha_binned_diff) = t.get_rmap_ps(rmap_diff, Nfreqchunks=4, m_alpha=2, m_freq=2, padding=1, window_fn=np.blackman)
         f = t.plot_rmap_ps_slice(ps_binned_no_error, ps_binned_with_error, k_modes_no_error, alpha_binned_no_error, alpha_idx_source=[], 
-                                 alpha_idx_no_source=[t.Npix//2, t.Npix//2+t.Npix//256, t.Npix//2+t.Npix//128, t.Npix//2+t.Npix//64], Nfreqchunks=4, difference_ps_binned=difference_ps_binned)
+                                 alpha_idx_no_source=[t.Npix//2, t.Npix//2+t.Npix//512, t.Npix//2+t.Npix//256, t.Npix//2+t.Npix//128], Nfreqchunks=4, difference_ps_binned=difference_ps_binned)
         f.savefig(path)
         plt.close(f)
 
