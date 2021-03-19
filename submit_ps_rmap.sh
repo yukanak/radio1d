cacff105457e6740699a2cd6f4a9b27f52aa773e
Yuka Nakato <nakat013@umn.edu>
2021-03-18 20:05:30 -0700

    For parallelizing the rmap ps plot generation; no filtering

---
+++ submit_ps_rmap.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+for npix in 2048 4096 8192; do
+    for redundant in 'True' 'False'; do
+        for sky in 'uniform' 'poisson' 'gaussian'; do
+            for seed in 0 1 2; do
+                for error in '1e-12' '10e-12' '100e-12' '300e-12' '1e-9'; do
+                    for correlated in 'True' 'False'; do
+                        for ndishes in 32 64 128; do
+                            #echo "bsub -C 0 -W 1:00 -R select[rhel60] -o logfile_${npix}_${redundant}_${sky}_${error}.txt plot_random_sky_slices.py $npix $redundant $sky $error"
+                            bsub -q long -C 0 -W 2:00 -R select[rhel60] -o logfile_${npix}_${redundant}_${sky}_${seed}_${error}_${correlated}_${ndishes}.txt ./plot_random_sky_slices_ps_rmap.py $npix $redundant $sky $seed $error $correlated $ndishes
+                        done
+                    done
+                done
+            done
+        done
+    done
+done
